eddc2ffc39078dc3a7092abe07a465e1144223a0
Alara <0931871@hr.nl>
2018-05-03 19:26:53 +0200

    For Loop
    Ik heb de code van opdr 2 in twee verschillende For-Loops gestopt. Waardoor de 100 visjes nu allemaal een aparte kleur hebben plus een bijhorende aparte positie.
    
    Bij de bubbels heb ik het zelfde gedaan.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,52 @@
-let screenSize = window.innerWidth * 0.9
-let screenSize2 = window.innerHeight * 0.8
+//
+//For-Loop die 100 visjes creeert
+//
+let i;
+for (i = 0; i < 100; i++) {
+    //console.log(i)
 
-let randomNumber = Math.random()* screenSize
-let randomNumber2 = Math.random()*screenSize2
-let randomNumber3 = Math.random()* screenSize
+    //Random nummer voor de scherm grootte
+    let screenSize = window.innerWidth * 0.9
+    let screenSize2 = window.innerHeight * 0.8
+
+    let randomNumber = Math.random() * screenSize
+    let randomNumber2 = Math.random() * screenSize2
+    let randomNumber3 = Math.random() * 360
+
+    //Vis creeeren
+    let fishes = document.createElement("fish")         //Creert vis in de DOM
+    document.body.appendChild(fishes)                   //Zorgt dat de vis in de body van de DOM gestopt word
+
+    //Posities/locaties
+    fishes.style.left = randomNumber + "px"
+    fishes.style.top = randomNumber2 + "px"
+
+    //fishes.style.webkitFilter = "hue-rotate(200deg)"
+    fishes.style.filter = "hue-rotate("+randomNumber3 +"deg)" //Binnen  0 - 360
+}
 
 //
-// geef de vis een random positie en een random kleur
+//For-Loop die 10 bubbles creert
 //
-let fishes = document.createElement("fish")
-document.body.appendChild(fishes)
-fishes.style.left = randomNumber + "px"
-fishes.style.top = randomNumber2 + "px"
-//fishes.style.webkitFilter = "hue-rotate(200deg)"
-fishes.style.filter = "hue-rotate(180deg)" //Binnen  0 - 360
+let q;
+for (q = 0; q<100; q++) {
+    //Random nummer voor de scherm grootte
+    let screenSize = window.innerWidth * 0.9
+    let screenSize2 = window.innerHeight
 
-// let fish = fishes[0] //Verander hier de nummer en de vis komt ergens anders, bel komt midden in scherm & vis kleur veranderd
-//fish.style.left = "200px"
-//fish.style.top = "100px"
+    let randomNumber4 = Math.random() * screenSize      //links-rechts
+    let randomNumber5 = Math.random() * screenSize2     //Hoogte
 
+    //Bubbel creeeren
+    let bubbles = document.createElement("bubble")      //Creert bubbel via DOM
+    document.body.appendChild(bubbles)                  //Zorgt dat de bubbel in de body van de DOM bevind
 
-//
-// geef de bubble een random positie
-//
-let bubbles = document.createElement("bubble")
-document.body.appendChild(bubbles)
-bubbles.style.left = randomNumber3 +"px"
-bubbles.style.top = "0px"
+    //Posities/Locaties
+    bubbles.style.left = randomNumber4 + "px"
+    bubbles.style.top = randomNumber5 + "px"
+}
 
 // let bubble = bubbles[0]// Verander dit en de bel komt midden in het beeld
-
 
 
 
