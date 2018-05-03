1693b8cc092bb92e135d561511b8d9ee43b15920
Alara <0931871@hr.nl>
2018-05-03 17:44:13 +0200

    sds

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //
 // geef de vis een random positie en een random kleur
 //
+let fishes = document.createElement("fish")
+document.body.appendChild(fishes)
 
 let fish = fishes[0] //Verander hier de nummer en de vis komt ergens anders, bel komt midden in scherm & vis kleur veranderd
 fish.style.left = "200px"
@@ -11,17 +13,16 @@ fish.style.filter = "hue-rotate(200deg)"
 //
 // geef de bubble een random positie
 //
+let bubbles = document.createElement("bubble")
+document.body.appendChild(bubbles)
 
 let bubble = bubbles[0]// Verander dit en de bel komt midden in het beeld
 bubble.style.left = "200px"
 bubble.style.top = "0px" //Hoog getal = kleur vis veranderd en geen bel
 
 
-let element = document.createElement("bubble")
-document.body.appendChild(element)
 
-let fishh = document.createElement("fishh")
-document.body.appendChild(fishh)
+
 
 //
 // roep een functie aan als alles geladen is
