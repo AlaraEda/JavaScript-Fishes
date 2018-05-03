a26aae4a229dbbb240fe71365d3c81a5923e0866
Alara <0931871@hr.nl>
2018-05-03 16:30:02 +0200

    Bubble verweiderd en toegevoegd

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // geef de vis een random positie en een random kleur
 //
 let fishes = document.getElementsByTagName("fish")
-let fish = fishes[0]
+let fish = fishes[0] //Verander hier de nummer en de vis komt ergens anders, bel komt midden in scherm & vis kleur veranderd
 fish.style.left = "200px"
 fish.style.top = "100px"
 fish.style.webkitFilter = "hue-rotate(200deg)"
@@ -12,13 +12,13 @@ fish.style.filter = "hue-rotate(200deg)"
 // geef de bubble een random positie
 //
 let bubbles = document.getElementsByTagName("bubble")
-let bubble = bubbles[0]
+let bubble = bubbles[0]// Verander dit en de bel komt midden in het beeld
 bubble.style.left = "200px"
-bubble.style.top = "0px"
+bubble.style.top = "0px" //Hoog getal = kleur vis veranderd en geen bel
 
 //
 // roep een functie aan als alles geladen is
-//
+// Dit word nog niet uitgevoerd.
 window.addEventListener("load", function () {
     console.log("start het aquarium")
 })
