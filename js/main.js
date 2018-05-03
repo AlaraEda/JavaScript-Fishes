3d1e8cece532d4835f2568c0e56d732021c8b1eb
Alara <0931871@hr.nl>
2018-05-03 22:38:10 +0200

    iets

--- js/main.js
+++ js/main.js
@@ -1,52 +1,67 @@
+
 //
 //For-Loop die 100 visjes creeert
 //
-let i;
-for (i = 0; i < 100; i++) {
-    //console.log(i)
 
-    //Random nummer voor de scherm grootte
-    let screenSize = window.innerWidth * 0.9
-    let screenSize2 = window.innerHeight * 0.8
 
-    let randomNumber = Math.random() * screenSize
-    let randomNumber2 = Math.random() * screenSize2
-    let randomNumber3 = Math.random() * 360
+window.setTimeout(kip(),2000)
+window.setTimeout(kip(),2000)
+function kip(){
+    let i;
+    for (i = 0; i < 10; i++) {
 
-    //Vis creeeren
-    let fishes = document.createElement("fish")                 //Creert vis in de DOM
-    document.body.appendChild(fishes)                           //Zorgt dat de vis in de body van de DOM gestopt word
+        //Random nummer voor de scherm grootte & plaats de code hier zodat er steeds weer een random nummer komt
+        let screenSize = window.innerWidth * 0.9
+        let screenSize2 = window.innerHeight * 0.8
 
-    //Posities/locaties
-    fishes.style.left = randomNumber + "px"
-    fishes.style.top = randomNumber2 + "px"
+        let randomNumber = Math.random() * screenSize
+        let randomNumber2 = Math.random() * screenSize2
+        let randomNumber3 = Math.random() * 360
 
-    //fishes.style.webkitFilter = "hue-rotate(200deg)"
-    fishes.style.filter = "hue-rotate("+randomNumber3 +"deg)"   //Binnen  0 - 360
+
+        //Vis creeeren
+        let fishes = document.createElement("fish")                    //Creert vis in de DOM
+        document.body.appendChild(fishes)                              //Zorgt dat de vis in de body van de DOM gestopt word
+
+        //Posities/locaties
+        fishes.style.left = randomNumber + "px"
+        fishes.style.top = randomNumber2 + "px"
+
+        //Kleuren
+        //fishes.style.webkitFilter = "hue-rotate(200deg)"
+        fishes.style.filter = "hue-rotate(" + randomNumber3 + "deg)"   //Binnen  0 - 360
+    }
 }
 
+
+        //console.log(i)
+
+// setTimeout(function(){
+//     //
+// //For-Loop die 10 bubbles creert
+// //
+//     let q;
+//     for (q = 0; q<100; q++) {
+//         //Random nummer voor de scherm grootte
+//         let screenSize = window.innerWidth * 0.9
+//         let screenSize2 = window.innerHeight
 //
-//For-Loop die 10 bubbles creert
+//         let randomNumber4 = Math.random() * screenSize      //links-rechts
+//         let randomNumber5 = Math.random() * screenSize2     //Hoogte
 //
-let q;
-for (q = 0; q<100; q++) {
-    //Random nummer voor de scherm grootte
-    let screenSize = window.innerWidth * 0.9
-    let screenSize2 = window.innerHeight
+//         //Bubbel creeeren
+//         let bubbles = document.createElement("bubble")      //Creert bubbel via DOM
+//         document.body.appendChild(bubbles)                  //Zorgt dat de bubbel in de body van de DOM bevind
+//
+//         //Posities/Locaties
+//         bubbles.style.left = randomNumber4 + "px"
+//         bubbles.style.top = randomNumber5 + "px"
+//     }
+// },2000)
 
-    let randomNumber4 = Math.random() * screenSize      //links-rechts
-    let randomNumber5 = Math.random() * screenSize2     //Hoogte
 
-    //Bubbel creeeren
-    let bubbles = document.createElement("bubble")      //Creert bubbel via DOM
-    document.body.appendChild(bubbles)                  //Zorgt dat de bubbel in de body van de DOM bevind
 
-    //Posities/Locaties
-    bubbles.style.left = randomNumber4 + "px"
-    bubbles.style.top = randomNumber5 + "px"
-}
 
-// let bubble = bubbles[0]// Verander dit en de bel komt midden in het beeld
 
 
 //
