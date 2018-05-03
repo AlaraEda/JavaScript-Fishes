97c18135158b8c6bb1eae4338043a1e909bdea2d
Alara <0931871@hr.nl>
2018-05-03 23:59:53 +0200

    Fish & Bubbels
    De vis en bubbels komen allebei random 1 voor 1 op het scherm.

--- js/main.js
+++ js/main.js
@@ -1,71 +1,49 @@
-
 //
-//For-Loop die 100 visjes creeert
+//For-Loop die 100 visjes & bellen creeert
 //
-
-
-function doSetTimeOut(i){
-    setTimeout(function(){
-        console.log(i)
-        //Random nummer voor de scherm grootte & plaats de code hier zodat er steeds weer een random nummer komt
-        let screenSize = window.innerWidth * 0.9
-        let screenSize2 = window.innerHeight * 0.8
-
-        let randomNumber = Math.random() * screenSize
-        let randomNumber2 = Math.random() * screenSize2
-        let randomNumber3 = Math.random() * 360
-
-
-        //Vis creeeren
-        let fishes = document.createElement("fish")                    //Creert vis in de DOM
-        document.body.appendChild(fishes)                              //Zorgt dat de vis in de body van de DOM gestopt word
-
-        //Posities/locaties
-        fishes.style.left = randomNumber + "px"
-        fishes.style.top = randomNumber2 + "px"
-
-        //Kleuren
-        //fishes.style.webkitFilter = "hue-rotate(200deg)"
-        fishes.style.filter = "hue-rotate(" + randomNumber3 + "deg)"   //Binnen  0 - 360
-    }, i * 900)
-}
-
     let i;
     for (i = 0; i < 100; i++) {
-        doSetTimeOut(i)
+        setTimeout(function(){
+            //Random nummer voor de scherm grootte &
+            //plaats de code hierin zodat er steeds weer een random nummer komt
+            let screenSize = window.innerWidth * 0.9
+            let screenSize2 = window.innerHeight * 0.8
+
+            let randomNumber = Math.random() * screenSize
+            let randomNumber2 = Math.random() * screenSize2
+            let randomNumber3 = Math.random() * 360
+
+            //Vis creeeren
+            let fishes = document.createElement("fish")                    //Creert vis in de DOM
+            document.body.appendChild(fishes)                              //Zorgt dat de vis in de body van de DOM gestopt word
+
+            //Posities/locaties
+            fishes.style.left = randomNumber + "px"
+            fishes.style.top = randomNumber2 + "px"
+
+            //Kleuren
+            //fishes.style.webkitFilter = "hue-rotate(200deg)"
+            fishes.style.filter = "hue-rotate(" + randomNumber3 + "deg)"   //Binnen  0 - 360
+        }, i* 1000)
+
+        setTimeout(function(){
+            //Random nummer voor de scherm grootte
+            let screenSize = window.innerWidth * 0.9
+            let screenSize2 = window.innerHeight
+
+            let randomNumber4 = Math.random() * screenSize                  //links-rechts
+            let randomNumber5 = Math.random() * screenSize2                 //Hoogte
+
+            //Bubbel creeeren
+            let bubbles = document.createElement("bubble")                   //Creert bubbel via DOM
+            document.body.appendChild(bubbles)                               //Zorgt dat de bubbel in de body van de DOM bevind
+
+            //Posities/Locaties
+            bubbles.style.left = randomNumber4 + "px"
+            bubbles.style.top = randomNumber5 + "px"
+        }, i * 100)                                                          //Om de 0,1 sec een nieuwe bel.
+
     }
-
-
-
-        //console.log(i)
-
-// setTimeout(function(){
-//     //
-// //For-Loop die 10 bubbles creert
-// //
-//     let q;
-//     for (q = 0; q<100; q++) {
-//         //Random nummer voor de scherm grootte
-//         let screenSize = window.innerWidth * 0.9
-//         let screenSize2 = window.innerHeight
-//
-//         let randomNumber4 = Math.random() * screenSize      //links-rechts
-//         let randomNumber5 = Math.random() * screenSize2     //Hoogte
-//
-//         //Bubbel creeeren
-//         let bubbles = document.createElement("bubble")      //Creert bubbel via DOM
-//         document.body.appendChild(bubbles)                  //Zorgt dat de bubbel in de body van de DOM bevind
-//
-//         //Posities/Locaties
-//         bubbles.style.left = randomNumber4 + "px"
-//         bubbles.style.top = randomNumber5 + "px"
-//     }
-// },2000)
-
-
-
-
-
 
 //
 // roep een functie aan als alles geladen is
