52cf97effef44fa81d10de8c5216ce073c3008b0
Alara <0931871@hr.nl>
2018-05-03 17:35:52 +0200

    bla

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //
 // geef de vis een random positie en een random kleur
 //
-let fishes = document.getElementsByTagName("fish")
+
 let fish = fishes[0] //Verander hier de nummer en de vis komt ergens anders, bel komt midden in scherm & vis kleur veranderd
 fish.style.left = "200px"
 fish.style.top = "100px"
@@ -11,10 +11,17 @@ fish.style.filter = "hue-rotate(200deg)"
 //
 // geef de bubble een random positie
 //
-let bubbles = document.getElementsByTagName("bubble")
+
 let bubble = bubbles[0]// Verander dit en de bel komt midden in het beeld
 bubble.style.left = "200px"
 bubble.style.top = "0px" //Hoog getal = kleur vis veranderd en geen bel
+
+
+let element = document.createElement("bubble")
+document.body.appendChild(element)
+
+let fishh = document.createElement("fishh")
+document.body.appendChild(fishh)
 
 //
 // roep een functie aan als alles geladen is
