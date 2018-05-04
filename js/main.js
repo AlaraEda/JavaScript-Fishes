2d90fc6d2a79803e80afef0015c8ed786a7f9295
Alara <0931871@hr.nl>
2018-05-04 20:59:04 +0200

    Comments
    Extra Comments voor Extra verduidelijking

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@
 //
     let i;
     for (i = 0; i < 100; i++) {
+        //Vis vertraging
         setTimeout(function(){
-            //Random nummer voor de scherm grootte &
-            //plaats de code hierin zodat er steeds weer een random nummer komt
+            //ScreenSize geeft de lengte en breedte aan van het scherm
             let screenSize = window.innerWidth * 0.9
             let screenSize2 = window.innerHeight * 0.8
 
+            //Math.Random() zorgt voor een getal tussen de 0-1.
+            //Door Math.Random() * screensize te doen zorgt Math Random voor een groter getal.
             let randomNumber = Math.random() * screenSize
             let randomNumber2 = Math.random() * screenSize2
             let randomNumber3 = Math.random() * 360
@@ -29,22 +31,23 @@
                                                                            //Plus je veranderd de vis eigenschap. Dus moet je fishes doen.
                 }
 
-            //Posities/locaties
+            //Posities/locaties vis
             fishes.style.left = randomNumber + "px"
             fishes.style.top = randomNumber2 + "px"
 
-            //Kleuren
+            //Kleuren van de vis
             fishes.style.webkitFilter = "hue-rotate("+ randomNumber3+ "deg)"
             fishes.style.filter = "hue-rotate(" + randomNumber3 + "deg)"   //Binnen  0 - 360
-        }, i* 100)
+        }, i* 100)                                                          //Om de 0,1 sec een nieuwe vis.
 
+        //Bel vertraging
         setTimeout(function(){
-            //Random nummer voor de scherm grootte
-            let screenSize = window.innerWidth * 0.9
-            let screenSize2 = window.innerHeight
+            //Random nummer voor de scherm grootte in breedte en hoogte
+            let screenSize = window.innerWidth * 0.9                        //Scherm breedte
+            let screenSize2 = window.innerHeight                            //Scherm hoogte
 
-            let randomNumber4 = Math.random() * screenSize                  //links-rechts
-            let randomNumber5 = Math.random() * screenSize2                 //Hoogte
+            let randomNumber4 = Math.random() * screenSize                  //Random getal in Scherm Breedte
+            let randomNumber5 = Math.random() * screenSize2                 //Random getal in Scherm Hoogte
 
             //Bubbel creeeren
             let bubbles = document.createElement("bubble")                   //Creert bubbel via DOM
